c115e699df7a024ebac64420034dfdf89f9e0747
TechnoZombie <124879219+TechnoZombie@users.noreply.github.com>
2024-09-02 23:42:26 +0100

    image fix

--- src/components/MainBoard.jsx
+++ src/components/MainBoard.jsx
@@ -14,7 +14,7 @@ function MainBoard({ fact, loading, fetchNewFact }) {
             <div className='outerFactBox'>
               <div className='chuckHeadBox'>
               <img
-                src="/pixelchuck.png"
+                src="./pixelchuck.png"
                 alt="Pixel Chuck Norris"
                 id='pixelchuck' />
                 </div>
